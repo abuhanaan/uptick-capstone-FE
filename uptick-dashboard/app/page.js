1855faff91dcd8f91534761d5cb2b8c1260d5aa1
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-20 13:59:27 +0100

    style: Update homepage navbar styles

--- uptick-dashboard/app/page.js
+++ uptick-dashboard/app/page.js
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col p-6 bg-[070C19]">
-            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-                <Image src='/images/navbar-logo.png' width={202} height={32} alt='Uptick logo' />
+        <main className="flex min-h-screen flex-col bg-[#070C19]">
+            <div className="flex h-20 items-center py-4 md:py-6 px-10 md:px-14">
+                <Image src='/images/navbar-logo.png' width={161} height={25} alt='Uptick logo' />
             </div>
         </main>
     )
