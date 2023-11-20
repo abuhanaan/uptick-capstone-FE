a667137b0079d79b32431fa6548820bd868886a6
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-20 14:11:39 +0100

    style: Style navbar with daisyUI

--- uptick-dashboard/app/page.js
+++ uptick-dashboard/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col bg-[#070C19]">
-            <div className="flex h-20 items-center py-4 md:py-6 px-10 md:px-14">
+            <div className="navbar py-6 px-10 md:px-14">
                 <Image src='/images/navbar-logo.png' width={161} height={25} alt='Uptick logo' />
             </div>
         </main>
