34fa47b59e17681957429abf67ca7d5c74ce077b
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-11-30 23:53:31 +0100

    refactor: Create a custom modal for applicants info

--- uptick-dashboard/app/dashboard/programs/map/[slug]/page.js
+++ uptick-dashboard/app/dashboard/programs/map/[slug]/page.js
@@ -1,9 +1,12 @@
+'use client';
+
 import Link from 'next/link';
 import { LiaFileDownloadSolid } from "react-icons/lia";
 import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
 import clsx from 'clsx';
-import ShowModalBtn from '../../show-modal-btn';
-import ModalBox from '../../modal-box';
+import ViewDetailsBtn from '../../../../components/view-details-btn';
+import Modal from '../../../../components/modal';
+import { useState } from 'react';
 
 const Program = ({ params }) => {
     const applicants = [
@@ -16,6 +19,8 @@ const Program = ({ params }) => {
         { id: 7, name: 'Ahmad', track: 'Mobile', status: 'Pending', date: '12/12/2022' },
         { id: 8, name: 'James', track: 'Fullstack', status: 'Accepted', date: '12/12/2022' },
     ];
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const programTitle = params.slug.split('-').join(' ');
 
@@ -66,7 +71,7 @@ const Program = ({ params }) => {
                                         </td>
                                         <td className="">{applicant.date}</td>
                                         <th className="text-end">
-                                                <ShowModalBtn />
+                                            <ViewDetailsBtn toggleModal={setIsModalOpen} />
                                         </th>
                                     </tr>
                                 ))
@@ -85,9 +90,73 @@ const Program = ({ params }) => {
             </div>
 
             {/* Applicant's Details Modal */}
-            <dialog id="applicant-details-modal" className="modal">
-                <ModalBox />
-            </dialog>
+            <Modal isOpen={isModalOpen} toggleModal={setIsModalOpen}>
+                <div className="flex flex-col gap-y-3 pb-3">
+                    <button className="btn bg-[#999999] text-white self-start">
+                        Download Resume/CV
+                    </button>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Full Name</h3>
+                        <p className="text-[#5988FF]">Oreoluwa Christopher</p>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Email</h3>
+                        <p className="text-[#5988FF]">oreoluwa@yahoo.com</p>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Phone Number</h3>
+                        <p className="text-[#5988FF]">+234 901 777 9568</p>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Current company</h3>
+                        <p className="text-[#5988FF]">CitiServe, Lagos</p>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">LinkedIn URL</h3>
+                        <Link href='https://linkedin.com/in/oreoluwa' className="text-[#5988FF]">https://linkedin.com/in/oreoluwa</Link>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Twitter URL</h3>
+                        <Link href='https://twitter.com/oreoluwa' className="text-[#5988FF]">https://twitter.com/oreoluwa</Link>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Github URL</h3>
+                        <Link href='https://github.com/oreoluwa' className="text-[#5988FF]">https://github.com/oreoluwa</Link>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Portfolio URL</h3>
+                        <Link href='https://portfolio.com' className="text-[#5988FF]">https://portfolio.com</Link>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1">Other Websites</h3>
+                        <Link href='https://otherwebsites.com' className="text-[#5988FF]">https://otherwebsites.com</Link>
+                    </div>
+
+                    <div className="text-sm">
+                        <h3 className="mb-1 capitalize">Additional Information</h3>
+                        <p className="text-[#5988FF]">Information that will make you stand out and prioritized over other applicants... Information that will make you stand out and prioritized over other applicants... Information that will make you stand out and prioritized over other applicants... Information that will make you stand out and prioritized over other applicants...</p>
+                    </div>
+
+                    <div className="flex items-center gap-4 text-sm mt-4">
+                        <div className="flex items-center gap-x-2">
+                            <input type="radio" name="radio-1" className="radio" /> Accept
+                        </div>
+
+                        <div className="flex items-center gap-x-2">
+                            <input type="radio" name="radio-1" className="radio" /> Reject
+                        </div>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
