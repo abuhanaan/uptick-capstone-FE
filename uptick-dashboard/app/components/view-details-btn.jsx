f682fb20299af700f5949fb0635767b8fd47e513
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-05 22:18:19 +0100

    refactor: Add children prop to ViewDetailsButton

--- uptick-dashboard/app/components/view-details-btn.jsx
+++ uptick-dashboard/app/components/view-details-btn.jsx
@@ -1,8 +1,8 @@
 'use client';
 
-const ViewDetailsBtn = ({ toggleModal }) => {
+const ViewDetailsBtn = ({ children, toggleModal }) => {
     return (
-        <button className="bg-[#B5CAFF] px-2 py-3 font-normal" onClick={() => toggleModal(prev => !prev)}>View Details</button>
+        <button className="bg-[#B5CAFF] px-2 py-3 font-normal" onClick={() => toggleModal(prev => !prev)}>{children}</button>
     )
 }
 
